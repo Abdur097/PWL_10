427be6205946990c3e5aa8349b36c4df6d22e7fe
Abdur097 <1941720094@student.polinema.ac.id>
2021-04-13 21:59:41 +0700

    membuat resource di route web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+    Route::resource('articles', ArticleController::class);
 });
